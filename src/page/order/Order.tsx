b2995998a15f0e4fd970d1ac237ed1802b7d3d0f
hanhuizhu <hanhuizhuxyl@gmail.com>
2020-07-15 13:50:12 +0800

    feat: 订单首页完成

--- src/page/order/Order.tsx
+++ src/page/order/Order.tsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import Header from '../../components/Header';
 import Icon from '../../components/iconfont';
+import OrderCard from './components/OrderCard';
+import getImage from '../../server/getImage';
 import './Order.scss';
 
 export default function Order() {
@@ -9,6 +11,48 @@ export default function Order() {
 
     // data
     const navs = ['全部订单', '进行订单', '完成订单'];
+    const orders = [
+        {
+            id: 1,
+            orderNum: '20201949520',
+            status: '代付款',
+            cover: getImage('home/good-cover.png'),
+            title: 'WIS男士控油祛痘洁面乳清爽男生洗面奶保湿去黑头',
+            subtitle: '黑色:M码',
+            count: 1,
+            price: 230,
+        },
+        {
+            id: 2,
+            orderNum: '20201949520',
+            status: '代付款',
+            cover: getImage('home/good-cover.png'),
+            title: 'WIS男士控油祛痘洁面乳清爽男生洗面奶保湿去黑头',
+            subtitle: '黑色:M码',
+            count: 1,
+            price: 230,
+        },
+        {
+            id: 3,
+            orderNum: '20201949520',
+            status: '代付款',
+            cover: getImage('home/good-cover.png'),
+            title: 'WIS男士控油祛痘洁面乳清爽男生洗面奶保湿去黑头',
+            subtitle: '黑色:M码',
+            count: 1,
+            price: 230,
+        },
+        {
+            id: 4,
+            orderNum: '20201949520',
+            status: '代付款',
+            cover: getImage('home/good-cover.png'),
+            title: 'WIS男士控油祛痘洁面乳清爽男生洗面奶保湿去黑头',
+            subtitle: '黑色:M码',
+            count: 1,
+            price: 230,
+        },
+    ];
 
     // handles
     const onNavChange = index => setNavIndex(index);
@@ -20,12 +64,19 @@ export default function Order() {
             <nav className="flex-row-between-center navs">
                 {navs.map((nav, index) => (
                     <span
+                        key={nav}
                         className={`nav ${navIndex === index ? 'nav-active' : ''}`}
                         onClick={() => onNavChange(index)}>
                         {nav}
                     </span>
                 ))}
             </nav>
+            {/* order */}
+            <div className="flex-column orders">
+                {orders.map(order => (
+                    <OrderCard key={order.id} goods={order} />
+                ))}
+            </div>
         </article>
     );
 }
--- src/page/order/components/OrderCard.tsx
+++ src/page/order/components/OrderCard.tsx
@@ -4,10 +4,35 @@ import './OrderCard.scss';
 export default function OrderCard(props) {
     const { goods = {} } = props;
     return (
-        <article className="Order-OrderCard">
-            <div>
-                <span>订单编号:{goods.orderNum}</span>
-                <span>{goods.status}</span>
+        <article className="flex-column Order-OrderCard">
+            <div className="flex-row-between-center card-header">
+                <span className="time">订单编号:{goods.orderNum}</span>
+                <span className="status">{goods.status}</span>
+            </div>
+            <div className="flex-row card-body">
+                <img src={goods.cover} className="cover" />
+                <div className="flex-column">
+                    <span className="title">{goods.title}</span>
+                    <span className="subtitle">{goods.subtitle}</span>
+                    <span className="flex1" />
+                    <div className="flex-row-between-center">
+                        <span className="count">共{goods.count}件商品</span>
+                        <span className="flex1" />
+                        <span className="pre-price">合计</span>
+                        <span className="price">{goods.price}</span>
+                    </div>
+                </div>
+            </div>
+            <div className="flex-row-between-center card-footer">
+                <div className="labs">
+                    <span className="lab">自营</span>
+                    <span className="lab line">|</span>
+                    <span className="lab">包邮</span>
+                </div>
+                <div className="flex-row">
+                    <span className="button-left">取消订单</span>
+                    <span className="button-right">立即付款</span>
+                </div>
             </div>
         </article>
     );
