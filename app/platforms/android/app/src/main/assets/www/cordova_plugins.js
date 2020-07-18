52d004be520f822b010ffee23f7b09418642358e
hanhuizhu <hanhuizhuxyl@gmail.com>
2020-07-18 11:00:02 +0800

    feat: 新增修改ICON & Splash

--- app/platforms/android/app/src/main/assets/www/cordova_plugins.js
+++ app/platforms/android/app/src/main/assets/www/cordova_plugins.js
@@ -7,10 +7,19 @@ cordova.define('cordova/plugin_list', function(require, exports, module) {
       "clobbers": [
         "Wechat"
       ]
+    },
+    {
+      "id": "cordova-plugin-splashscreen.SplashScreen",
+      "file": "plugins/cordova-plugin-splashscreen/www/splashscreen.js",
+      "pluginId": "cordova-plugin-splashscreen",
+      "clobbers": [
+        "navigator.splashscreen"
+      ]
     }
   ];
   module.exports.metadata = {
     "cordova-plugin-whitelist": "1.3.4",
-    "cordova-plugin-wechat": "3.0.0"
+    "cordova-plugin-wechat": "3.0.0",
+    "cordova-plugin-splashscreen": "5.0.4"
   };
 });
